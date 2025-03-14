71e2154cb04eaa29f1f5ecc93084cd2d13725c7c
Yunus Msayib <yunus.msayib@gmail.com>
2025-03-14 00:44:11 +0000

    exploration page updated

--- pages/1_Load.py
+++ pages/1_Load.py
@@ -33,93 +33,108 @@ else:
     st.warning('No data')
 
 if run_proc_btn:
-    st.spinner('Running', show_time=True)
+    with st.spinner('Running', show_time=True):
+        #
+        #Load local model and tokenizer
+        #
+        import torch
+        device = 'cpu'; torch.set_num_threads(4) #mimic CPU-only
 
-    #
-    #Load local model and tokenizer
-    #
-    import torch
-    device = 'cpu'; torch.set_num_threads(4) #mimic CPU-only
+        from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
-    from transformers import AutoModelForSequenceClassification, AutoTokenizer
+        tokenizer = AutoTokenizer.from_pretrained('data/hfcheckpoint-tokenizer')
+        model = AutoModelForSequenceClassification.from_pretrained('data/hfcheckpoint-tinybert-sst5-fft')
+        
+        model.eval()
+        model.to(device)
 
-    tokenizer = AutoTokenizer.from_pretrained('../data/hfcheckpoint-tokenizer')
-    model = AutoModelForSequenceClassification.from_pretrained('../data/hfcheckpoint-tinybert-sst5-fft')
-    
-    model.eval()
-    model.to(device)
+        #
+        #FFT to Dataset.from_pandas
+        #
+        from model_utils import combine_question_answer
+        from datasets import Dataset
 
-    #
-    #FFT to Dataset.from_pandas
-    #
-    from model_utils import combine_question_answer
-    from datasets import Dataset
+        #Load from pandas
+        fft_dataset = Dataset\
+            .from_pandas(df_tweaked[['question_type', 'answer_clean']])\
+            .remove_columns('__index_level_0__')
+        
+        #Merge q & a into single text sequence
+        fft_dataset = fft_dataset\
+            .map(combine_question_answer, batched=True)\
+            .remove_columns(['question_type', 'answer_clean'])
+        
+        #Tokenize
+        fft_tokenized = fft_dataset\
+            .map(lambda batch: tokenizer(batch['q_and_a'], truncation=True, max_length=512), batched=True)\
+            .remove_columns('q_and_a')
+        
+        #
+        # Configure loader
+        #
+        from torch.utils.data import DataLoader
+        from transformers import DataCollatorWithPadding
+        from scipy.special import softmax
 
-    #Load from pandas
-    fft_dataset = Dataset\
-        .from_pandas(df_tweaked[['question_type', 'answer_clean']])\
-        .remove_columns('__index_level_0__')
-    
-    #Merge q & a into single text sequence
-    fft_dataset = fft_dataset\
-        .map(combine_question_answer, batched=True)\
-        .remove_columns(['question_type', 'answer_clean'])
-    
-    #Tokenize
-    fft_tokenized = fft_dataset\
-        .map(lambda batch: tokenizer(batch['q_and_a'], truncation=True, max_length=512), batched=True)\
-        .remove_columns('q_and_a')
-    
-    #
-    # Configure loader
-    #
-    from torch.utils.data import DataLoader
-    from transformers import DataCollatorWithPadding
-    from scipy.special import softmax
+        dynamic_padding_collator = DataCollatorWithPadding(tokenizer)
 
-    dynamic_padding_collator = DataCollatorWithPadding(tokenizer)
+        loader = DataLoader(
+            fft_tokenized,  batch_size=32,
+            pin_memory=False if device=='cpu' else True,
+            shuffle=False, collate_fn=dynamic_padding_collator
+        )
 
-    loader = DataLoader(
-        fft_tokenized,  batch_size=32,
-        pin_memory=False if device=='cpu' else True,
-        shuffle=False, collate_fn=dynamic_padding_collator
-    )
+        #
+        # Get embeddings
+        # For efficiency, get embeddings & logits in a single pass aot separate passes
+        #
+        embeddings = []
+        logits = []
 
-    #
-    # Get embeddings
-    # For efficiency, get embeddings & logits in a single pass aot separate passes
-    #
-    embeddings = []
-    logits = []
+        classifier_head = torch.nn.Sequential(model.dropout, model.classifier)
+        with torch.no_grad():
+            for minibatch in loader:
+                minibatch = {k: v.to(device) for k, v in minibatch.items()}
 
-    classifier_head = torch.nn.Sequential(model.dropout, model.classifier)
-    with torch.no_grad():
-        for minibatch in loader:
-            minibatch = {k: v.to(device) for k, v in minibatch.items()}
+                features = model.bert(
+                    input_ids=minibatch['input_ids'],
+                    attention_mask=minibatch['attention_mask']
+                )
 
-            features = model.bert(
-                input_ids=minibatch['input_ids'],
-                attention_mask=minibatch['attention_mask']
-            )
+                #[B, L, emb] -> avg tokens' hidden rep -> [B, emb]
+                # embeddings_mb = features.last_hidden_state.cpu().numpy().mean(axis=1)
 
-            #[B, L, emb] -> avg tokens' hidden rep -> [B, emb]
-            # embeddings_mb = features.last_hidden_state.cpu().numpy().mean(axis=1)
+                #[B, L, emb] -> final CLS rep -> [B, emb]
+                embeddings_mb = features.pooler_output.cpu().numpy()
 
-            #[B, L, emb] -> final CLS rep -> [B, emb]
-            embeddings_mb = features.pooler_output.cpu().numpy()
+                #Pass remainder through for logits
+                logits_mb = classifier_head(features.pooler_output)
 
-            #Pass remainder through for logits
-            logits_mb = classifier_head(features.pooler_output)
+                #Record results
+                embeddings.append(embeddings_mb)
+                logits.append(logits_mb)
+        
+        embeddings = np.concatenate(embeddings, axis=0)
+        logits = np.concatenate(logits, axis=0)
 
-            #Record results
-            embeddings.append(embeddings_mb)
-            logits.append(logits_mb)
-    
-    embeddings = np.concatenate(embeddings, axis=0)
-    logits = np.concatenate(logits, axis=0)
+        #
+        # From logits, get: predictions, entropies, class probabilities
+        #
+        probabilities = softmax(logits, axis=1)
+        
+        predictions = logits.argmax(axis=1)
+        class_probs = probabilities[np.arange(len(logits)), predictions]
+        entropies = -(probabilities * np.log2(probabilities)).sum(axis=1)
+    st.success('Done!')
 
+    st.session_state.data_dict = {
+        'df_tweaked': df_tweaked,
+        'embeddings': embeddings,
+        'predictions': predictions,
+        'class_probs': class_probs,
+        'entropies': entropies,
+    }
 
-    
 
 
 
--- pages/2_Explore.py
+++ pages/2_Explore.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
@@ -10,11 +11,29 @@ from plotly.subplots import make_subplots
 st.title('Explore')  #st.header
 st.write('Explore the scatter plots with different column colourings')
 
-if hasattr(st.session_state, 'df_tweaked'):
-    df = st.session_state.df_tweaked
+col1, col2, col3 = st.columns(3)
+with col1:
+    neigh_slider = st.slider('local vs global relations', min_value=3, max_value=50, value=15, step=2)
+with col2:
+    size_slider = st.slider('marker size', min_value=1, max_value=10, value=3, step=1)
+with col3:
+    opacity_slider = st.slider('opacity', min_value=0.1, max_value=1., value=1., step=0.1)
+
+if hasattr(st.session_state, 'data_dict'):
+    from umap import UMAP
+
+    df = st.session_state.data_dict['df_tweaked']
+
+    proj = UMAP(neigh_slider, random_state=0).fit_transform(st.session_state.data_dict['embeddings'])
+    proj_x, proj_y = proj.T
+    
+    predictions = st.session_state.data_dict['predictions']
+    class_probs = st.session_state.data_dict['class_probs']
+    entropies = st.session_state.data_dict['entropies'] #/ np.log2(5) * 100
+    descriptions = [{0: 'very positive', 1: 'positive', 2: 'neutral', 3: 'negative', 4: 'very negative'}[p] for p in predictions]
     
     fig = make_subplots(
-        rows=2, cols=2,
+        rows=3, cols=1,
         #shared_xaxes=True, shared_yaxes=True, #latter not quite as needed
         
         # specs=[
@@ -28,13 +47,70 @@ if hasattr(st.session_state, 'df_tweaked'):
         # horizontal_spacing=0.05, vertical_spacing=0.05
     )
 
-    fig.add_trace(go.Scatter(x=df['x'], y=df['y'], mode='markers', marker=dict(color=df['column1'])), row=1, col=1)
-    fig.add_trace(go.Scatter(x=df['x'], y=df['y'], mode='markers', marker=dict(color=df['column2'])), row=1, col=2)
-    fig.add_trace(go.Scatter(x=df['x'], y=df['y'], mode='markers', marker=dict(color=df['column3'])), row=2, col=1)
-    fig.add_trace(go.Scatter(x=df['x'], y=df['y'], mode='markers', marker=dict(color=df['column4'])), row=2, col=2)
+    hovertext = [f'{desc} <br> {ans}' for desc, ans in zip(descriptions, df.answer_clean)]
+    scatter_props = dict(x=proj_x, y=proj_y, hovertext=hovertext, mode='markers', opacity=opacity_slider)
 
+    scatter1 = go.Scatter(
+        **scatter_props,
+        marker=dict(
+            color=predictions,
+            colorscale='PiYG_r',
+            size=size_slider,
+            colorbar={
+                'title': 'sentiment',
+                'lenmode': 'fraction',
+                'len': 0.3,
+                'y': 0.9,
+                'tickmode': 'array',
+                'tickvals': [0, 1, 2, 3, 4],
+                'ticktext': ['very positive', 'positive', 'neutral', 'negative', 'very negative']
+            },
+            #/colorbar
+        ),
+        #/marker
+    )
+
+    scatter2 = go.Scatter(
+        **scatter_props,
+        marker=dict(
+            color=class_probs,
+            colorscale='Reds',
+            size=size_slider,
+            colorbar={
+                'title': 'probability',
+                'lenmode': 'fraction',
+                'len': 0.3,
+                'y': 0.55,
+            },
+            #/colorbar
+        ),
+        #/marker
+    )
+
+    scatter3 = go.Scatter(
+        **scatter_props,
+        marker=dict(
+            color=entropies,
+            colorscale='Purples',
+            size=size_slider,
+            colorbar={
+                'title': 'entropy',
+                'lenmode': 'fraction',
+                'len': 0.3,
+                'y': 0.2,
+            },
+            #/colorbar
+        ),
+        #/marker
+    )
+    
+    #Layout
+    fig.add_trace(scatter1, row=1, col=1)
+    fig.add_trace(scatter2, row=2, col=1)
+    fig.add_trace(scatter3, row=3, col=1)
+    
     #manually link all to same x/y
-    for row, col in [[1, 1], [1, 2], [2, 1], [2, 2]]:
+    for row, col in [[1, 1], [2, 1], [3, 1]]:
         fig.update_xaxes(matches='x', row=row, col=col)
         fig.update_yaxes(matches='y', row=row, col=col)
 
@@ -45,7 +121,8 @@ if hasattr(st.session_state, 'df_tweaked'):
     fig.update_layout(
         title='',
         showlegend=False,
-        height=600, width=1200
+        height=1000, #width=100
     )
 
-    st.plotly_chart(fig)
+    st.plotly_chart(fig, use_container_width=True)
+
